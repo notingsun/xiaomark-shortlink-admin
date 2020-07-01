fbd1c0887b6899089c45507acdb888e612b632e6
youyou12344 <1060030338@qq.com>
2020-07-01 20:51:14 +0800

    fix: 访问次数 / 人数 / IP数

--- src/views/common/mixins-short-link.js
+++ src/views/common/mixins-short-link.js
@@ -211,7 +211,7 @@ export default {
           },
           {
             title: '访问次数 / 人数 / IP数',
-            minWidth: 200,
+            minWidth: 230,
             format: this.$global.utils.countFormat.three,
             render: (h, { row }) => {
               const stats = row.stats || {}
